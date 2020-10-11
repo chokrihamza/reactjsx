a1bb96bae239e18f4e8b007316726c81a5329c44
chokrihamza <chokrihamza1994@gmail.com>
2020-10-11 23:43:36 +0200

    check point jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style.css'
+import react from './images/react.png'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+  <div className="prime" style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+  <h1 className="title_red">Hello My name is Hamza Chokri </h1>
+  <br />
+  <div className="images">
+  <img src="./image/js.png" width={320} height={240} />
+  <br />
+  <img src={react} width={320} height={240} />
+  </div>
+  
+  </div>
+  <div className="video">
+  <iframe width={320} height={240} src="https://www.youtube.com/embed/NT299zIk2JY" ></iframe>
+  </div>
+  
+  </div>,document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
